30a19afd2be88e649f371c52defee5d0f6e270dc
FreDriCk-san <31565053+FreDriCk-san@users.noreply.github.com>
2018-09-24 12:14:14 +0700

    Add files via upload

---
+++ Message Async/SendAsync/SendAsync/Message.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SendAsync
+{
+    public class Message
+    {
+
+        public static void Create(BlockingCollection<string> bCollection)
+        {
+            var success = false;
+
+            // Simple message with GUID
+            var message = Guid.NewGuid().ToString();
+
+            try
+            {
+                success = bCollection.TryAdd(message, 2);
+            }
+            // Last version with canceltoken
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Stop creating messages");
+                bCollection.CompleteAdding();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Create Error: " + exception.Message);
+            }
+
+            // If sended successful, add to queue
+            if (success)
+            {
+                Console.WriteLine("Sending new message: " + message);
+                Console.WriteLine("//Collection current count: " + bCollection.Count);
+            }
+
+        }
+
+
+        public static void Receive(BlockingCollection<string> bCollection)
+        {
+            try
+            {
+                if (bCollection.TryTake(out string result, 2))
+                {
+                    Console.WriteLine("Message {0} was sended", result);
+                }
+            }
+            // Last version with canceltoken
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Stop sending messages");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Receive Error: " + exception.Message);
+            }
+        }
+
+
+        // Simulate sending statement
+        public static async Task Send()
+        {
+            try
+            {
+                var random = new Random().Next(4000);
+                await Task.Delay(random);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Sending Error: " + exception);
+            }
+        }
+
+    }
+}
---
+++ Message Async/SendAsync/SendAsync/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SendAsync
+{
+    class Program
+    {
+
+        private static void UI(BlockingCollection<string> bCollection)
+        {
+            for (; ; )
+            {
+                //Console.Clear();
+                //Console.WriteLine("Created : ");
+                //Console.WriteLine("Processing : " + bCollection.Count);
+                //Console.WriteLine("Received : ");
+            }
+        }
+
+
+        private static void CreateMessage(BlockingCollection<string> bCollection)
+        {
+            for (; ; )
+            {
+                Message.Create(bCollection);
+            }
+        }
+
+
+        private static void Processing(BlockingCollection<string> bCollection)
+        {
+            for (; ; )
+            {
+                var send = Task.Run(async () =>
+                {
+                    await Message.Send();
+                });
+
+                Message.Receive(bCollection);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            var bCollection = new BlockingCollection<string>();
+
+
+            var threadUI = new Thread(() => UI(bCollection));
+            var threadCreate = new Thread(() => CreateMessage(bCollection));
+            var threadProcess = new Thread(() => Processing(bCollection));
+
+            threadCreate.Start();
+            threadProcess.Start();
+            threadUI.Start();
+
+
+            Console.ReadKey();
+        }
+    }
+}
